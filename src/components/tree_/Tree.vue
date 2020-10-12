aab11e1fdc1af29d14a3e8adbfa8db87b10092ed
huangyunxin1994 <947593101@qq.com>
2020-10-12 10:28:58 +0800

    tree组件添加搜索功能

--- src/components/tree_/Tree.vue
+++ src/components/tree_/Tree.vue
@@ -1,36 +1,60 @@
 <template>
-    <a-tree
-      class="custom-tree"
-      :tree-data="dataSource"
-      :default-expanded-keys="['key-01']"
-      :selectedKeys="selectedKeys"
-      :replace-fields="replaceFields"
-      :block-node="blockNode"
-      @select="onSelect"
-    >
-        <a-icon slot="switcherIcon" type="down" />
-        <template slot="custom" slot-scope="item">
-            <span>{{ item.title }}{{ showCount ? '（'+item.count+'）' : ''}}</span>
-            <a-dropdown :trigger="['click']" v-if="allowEdit">
-                <a class="btn" @click="e => e.stopPropagation()"><a-icon type="ellipsis" /></a>
-                <a-menu slot="overlay">
-                    <a-menu-item key="1" @click="edit">编辑</a-menu-item>
-                    <a-menu-item key="2" @click="add">新增</a-menu-item>
-                    <a-menu-item key="3" @click="remove" v-if="item.key!='key-01'">删除</a-menu-item>
-                </a-menu>
-            </a-dropdown>
-            <a-tooltip>
-                <template slot="title">
-                   查看全部
-                </template>
-                <a class="btnReload" @click.stop="reload" v-if="allowReload&&item.key=='key-01'"><a-icon type="reload" /></a>
-            </a-tooltip>
-            
-        </template>
-    </a-tree>
+    <div>
+        <a-input-search v-if="allowSearch" style="margin-bottom: 8px" placeholder="Search" @change="onChange" />
+        <a-tree
+        class="custom-tree"
+        :expanded-keys="expandedKeys"
+        :auto-expand-parent="autoExpandParent"
+        :tree-data="dataSource"
+        :selectedKeys="selectedKeys"
+        :replace-fields="replaceFields"
+        :block-node="blockNode"
+        @select="onSelect"
+        @expand="onExpand"
+        >
+            <a-icon slot="switcherIcon" type="down" />
+            <template slot="custom" slot-scope="item">
+                <span v-if="item.title.indexOf(searchValue) > -1">
+                    {{ item.title.substr(0, item.title.indexOf(searchValue)) }}{{ showCount ? '（'+item.count+'）' : ''}}
+                <span style="color: #f50">{{ searchValue }}</span>
+                    {{ item.title.substr(item.title.indexOf(searchValue) + searchValue.length) }}{{ showCount ? '（'+item.count+'）' : ''}}
+                </span>
+                <span v-else>{{ item.title }}{{ showCount ? '（'+item.count+'）' : ''}}</span>
+                <a-dropdown :trigger="['click']" v-if="allowEdit">
+                    <a class="btn" @click="e => e.stopPropagation()"><a-icon type="ellipsis" /></a>
+                    <a-menu slot="overlay">
+                        <a-menu-item key="1" @click="edit">编辑</a-menu-item>
+                        <a-menu-item key="2" @click="add">新增</a-menu-item>
+                        <a-menu-item key="3" @click="remove" v-if="item.key!='key-01'">删除</a-menu-item>
+                    </a-menu>
+                </a-dropdown>
+                <a-tooltip>
+                    <template slot="title">
+                    查看全部
+                    </template>
+                    <a class="btnReload" @click.stop="reload" v-if="allowReload&&item.key=='key-01'"><a-icon type="reload" /></a>
+                </a-tooltip>
+                
+            </template>
+        </a-tree>
+    </div>
 </template>
 
 <script>
+const getParentKey = (key, tree) => {
+  let parentKey;
+  for (let i = 0; i < tree.length; i++) {
+    const node = tree[i];
+    if (node.children) {
+      if (node.children.some(item => item.key === key)) {
+        parentKey = node.key;
+      } else if (getParentKey(key, node.children)) {
+        parentKey = getParentKey(key, node.children);
+      }
+    }
+  }
+  return parentKey;
+};
 export default {
     props:{
       //树数据 类型：数组|Array
@@ -55,6 +79,11 @@ export default {
           type:Boolean,
           default:false
       },
+      //是否允许搜索 类型：布尔值|Boolean
+      allowSearch:{
+          type:Boolean,
+          default:true
+      },
       //
       replaceFields:{
           type:Object,
@@ -70,10 +99,22 @@ export default {
     data() {
         return {
             showLine: true,
-            selectedKeys:[]
+            selectedKeys:[],
+            dataList:[],
+            searchValue:'',
+            expandedKeys:[],
+            autoExpandParent:true
         };
     },
+    mounted(){
+        this.initData(this.dataSource)
+    },
     methods: {
+        onExpand(expandedKeys) {
+            console.log(expandedKeys)
+            this.expandedKeys = expandedKeys;
+            this.autoExpandParent = false;
+        },
         onSelect(selectedKeys) {
             this.selectedKeys = selectedKeys
             let params = {}
@@ -102,6 +143,37 @@ export default {
         reload(){
             this.selectedKeys=[]
             this.$emit('reloadTreeNode')
+        },
+        initData(data){
+            for (let i = 0; i < data.length; i++) {
+                const node = data[i];
+                const key = node.key;
+                const title = node.title;
+                this.dataList.push({ key, title });
+                if (node.children) {
+                    this.initData(node.children);
+                }
+            }
+            console.log(this.dataList)
+        },
+        //搜索
+        onChange(e) {
+            const value = e.target.value;
+            const expandedKeys = this.dataList
+                .map(item => {
+                if (value&&item.title.indexOf(value) > -1) {
+                    return getParentKey(item.key, this.dataSource);
+                }
+                return null;
+                })
+                .filter((item, i, self) => item && self.indexOf(item) === i);
+            console.log(expandedKeys)
+            Object.assign(this, {
+                expandedKeys,
+                searchValue: value,
+                autoExpandParent: true,
+            });
+            console.log(this.expandedKeys)
         }
     },
 };
